b9c89fbd9c91d2af8ec8d010061099eca61986a8
jiggy97 <dkswnsdud159@naver.com>
2023-05-08 23:56:17 +0900

    create spring boot project

---
+++ src/main/java/com/capston/senierproject/SenierProjectApplication.java
@@ -0,0 +1,13 @@
+package com.capston.senierproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenierProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenierProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capston/senierproject/SenierProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capston.senierproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenierProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
